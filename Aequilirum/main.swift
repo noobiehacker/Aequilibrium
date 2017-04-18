4269a67c6c9fc8746b489e3b6d31e38eb52bd2aa
David Chan <davidkwokhochan@gmail.com>
2017-04-17 23:45:37 -0700

    Initial Commit

---
+++ Aequilirum/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Aequilirum
+//
+//  Created by David Kwok Ho Chan on 4/17/17.
+//  Copyright © 2017 David Kwok Ho Chan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
